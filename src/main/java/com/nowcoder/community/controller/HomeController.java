5ce76068a95a1cc89b27ff801ac8d183bde78fba
cuizhiya <cuizhiya_hbu@163.com>
2024-04-10 16:46:10 +0800

    首次提交代码

---
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.*;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+
+    // 前端控制器会将model和page都创建出来，并且page已经自动放置在model中了
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page){
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        // 接收返回值
+        List<DiscussPost> discussPostList = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if(discussPostList != null){
+            for (DiscussPost discussPost : discussPostList) {
+                Map<String,Object> map = new HashMap<>();
+                map.put("post",discussPost);
+                User user = userService.findUserById(discussPost.getUserId());
+                map.put("user",user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts",discussPosts);
+        return "/index";
+    }
+}
